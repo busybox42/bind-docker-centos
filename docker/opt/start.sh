97c1d9d7a08ca7f983a825d23a5b8cc794059c16
Alan Denniston <ADenniston@synacor.com>
2018-03-31 14:18:30 -0400

    modifying start script

--- docker/opt/start.sh
+++ docker/opt/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/named -u named -c /etc/named.conf -f
+echo "Starting named..."
+exec /usr/sbin/named -u named -c /etc/named.conf -f
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
-fi
-
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
-fi
